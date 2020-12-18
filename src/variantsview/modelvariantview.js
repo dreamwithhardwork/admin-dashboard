cdb3fdb4b8ca547ff75132687416df007d7e2a0e
srikanth <srikanth2121098@gmail.com>
2020-12-18 12:05:55 +0530

    add changes

--- src/variantsview/modelvariantview.js
+++ src/variantsview/modelvariantview.js
@@ -1,103 +1,78 @@
-import { AirlineSeatReclineExtra, CheckCircleOutlineOutlined, CloseOutlined, DirectionsCar, ExpandMore, FlashOnOutlined, SpeedOutlined } from '@material-ui/icons';
+import { AirlineSeatReclineExtra, ArrowBack, CheckCircleOutlineOutlined, CloseOutlined, DirectionsCar, ExpandMore, FlashOnOutlined, SpeedOutlined } from '@material-ui/icons';
 import { Autocomplete } from '@material-ui/lab';
 import Carousel from 'react-material-ui-carousel';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { SERVICE_URL } from '../components/constants/constants';
-import { getRequest } from '../components/constants/headers';
 import SimpleBackdrop from '../components/messages/backdrop';
-import {getVariant} from '../components/variant/specifications/carpropertiesservices'
 import '../variantsview/modelview.css'
-const { Container, Paper, Card, CardActionArea, CardContent, CardMedia, Typography, ListItemText, TextField, Accordion, AccordionSummary, AccordionDetails, Divider } = require("@material-ui/core");
+const { Container, Paper, Card, CardActionArea, CardContent, CardMedia, Typography, ListItemText, TextField, Accordion, AccordionSummary, AccordionDetails, Divider, FormGroup, FormControlLabel } = require("@material-ui/core");
 const { Fragment, useState, useRef, useEffect } = require("react");
 
 
-const specifications =  [
-    {
-        "id": "5fbfeda637b2af5f45b4c831",
-        "name": "Engine ",
-        "properties": [
-            {
-                "name": "Engine Type",
-                "propertyDataType": "STRING"
-            },
-            {
-                "name": "Fast Charging",
-                "propertyDataType": "BOOLEAN",
-                "status": "KEY FEATURE",
-                "value": "YES"
-            },
-            {
-                "name": "Displacement (cc)",
-                "propertyDataType": "STRING",
-                "status": "KEY SPECIFICATION",
-                "value": "1197"
-            },
-            {
-                "name": "Max Power",
-                "propertyDataType": "STRING",
-                "value": "81.86bhp@6000rpm"
-            },
-            {
-                "name": "Max Torque",
-                "propertyDataType": "STRING",
-                "value": "114.74nm@4200rpm"
-            },
-            {
-                "name": "No. of cylinder",
-                "propertyDataType": "STRING"
-            },
-            {
-                "name": "Valves Per Cylinder",
-                "propertyDataType": "STRING"
-            },
-            {
-                "name": "Turbo Charger",
-                "propertyDataType": "BOOLEAN"
-            },
-            {
-                "name": "Battery Warranty",
-                "propertyDataType": "BOOLEAN"
-            },
-            {
-                "name": "Transmission Type",
-                "propertyDataType": "STRING"
-            },
-            {
-                "name": "Gear Box",
-                "propertyDataType": "STRING"
-            }
-        ]
-    },
-    {
-        "id": "5fbfee2437b2af5f45b4c833",
-        "name": "Fuel & Performance",
-        "properties": [
-            {
-                "name": "Fuel Type",
-                "propertyDataType": "STRING",
-                "value": "PETROL"
-            },
-            {
-                "name": "Mileage (ARAI)",
-                "propertyDataType": "STRING",
-                "status": "KEY SPECIFICATION",
-                "value": "20.35"
-            }
-        ]
-    }
-];
-
-
 function ModelVariantView(props) {
-
     const [expanded, setExpanded] = useState(false)
     const [backDrop,setBackDrop] = useState(false)
     const {name,variantName,fuelType} = useParams();
     const [model, setModel] = useState({});
     const [variant,setVariant] = useState({});
+    const history = useHistory();
+    const [bhp,setBHP] = useState("--");
+    const [engine,setengine] = useState("--");
+    const [mileage,setmileage] = useState("--");
+    const [variantList, setVariantList] = useState([]);
+
 
     const handleChange = (panel) => (event, isExpanded) => {
         setExpanded(isExpanded?panel:false);
+    }
+
+    const handleChangeVariant = (e,value) => {
+        let variant = value.split(" / ");
+        model.variants.map(item => {
+            if(item.variantName === variant[0] && variant[1] === item.fuelType){
+                setVariant(item);
+                if(Object.keys(item).length !== 0){
+                    if(item.specifications!==undefined && item.specifications!==null && item.specifications.length!==0){
+                        item.specifications.map(spec => {
+                            if(spec.properties!==null && spec.properties!==undefined && spec.properties.length!==0){
+                                spec.properties.map(property => {
+                                    if(property.name === "Max Power"){
+                                        try{
+                                            let n = Number.parseFloat(property.value.substr(0,property.value.indexOf("bhp")));
+                                            setBHP(n);
+                                        }catch(ex){
+                                            setBHP("--")
+                                        }
+                                        
+                            
+                                      }else if(property.name === "Displacement (cc)"){
+                                        try{
+                                            let n = Number.parseFloat(property.value);
+                                            setengine(n);
+                                        }catch(ex){
+                                            setengine("--")
+                                        }
+                            
+                                      } else if(property.name === "Mileage (ARAI)"){
+                                        try{
+                                            let n = Number.parseFloat(property.value);
+                                            setmileage(n);
+                                        }catch(ex){
+                                            setmileage("--")
+                                        }
+                                      }
+                                })
+                            }
+                            else{
+                                setBHP("--");setengine("--");setmileage("--")
+                            }
+                        })
+                    }else{
+                        setBHP("--");setengine("--");setmileage("--");
+                    }
+                }
+            }
+        })
     }
 
     useEffect(async ()=>{
@@ -106,15 +81,54 @@ function ModelVariantView(props) {
             let res = await fetch(SERVICE_URL.GET_MODEL_BY_NAME+name);
             let body = await res.json();
             setModel(body);
-
+            let variantToMap={};
+            let variantsLists = [];
             body.variants.map(item => {
-                if(item.variantName === variantName && fuelType == item.fuelType){
+                variantsLists.push(item.variantName+" / "+item.fuelType);
+                if(item.variantName === variantName && fuelType === item.fuelType){
                     setVariant(item);
+                    variantToMap = item;
                     console.log(item)
                 }
             })
-            if(Object.keys(variant).length===0){
-                setVariant(body.variants.length!==0?body.variants[0]:{})
+            setVariantList(variantsLists);
+            if(Object.keys(variantToMap).length===0){
+                variantToMap = body.variants.length!==0?body.variants[0]:{};
+                setVariant(variantToMap);
+            }
+            if(Object.keys(variantToMap).length !== 0){
+                if(variantToMap.specifications!==undefined && variantToMap.specifications!==null){
+                    variantToMap.specifications.map((spec,index) => {
+                        if(index === 0){
+                            setExpanded(spec.name)
+                        }
+                        if(spec.properties!==null && spec.properties!==undefined){
+                            spec.properties.map(property => {
+                                if(property.name === "Max Power"){
+                                    try{
+                                        let n = Number.parseFloat(property.value.substr(0,property.value.indexOf("bhp")));
+                                        setBHP(n);
+                                    }catch(ex){}
+                                    
+                        
+                                  }else if(property.name === "Displacement (cc)"){
+                                    try{
+                                        let n = Number.parseFloat(property.value);
+                                        setengine(n);
+                                    }catch(ex){}
+                        
+                                  } else if(property.name === "Mileage (ARAI)"){
+                                    try{
+                                        let n = Number.parseFloat(property.value);
+                                        setmileage(n);
+                                    }catch(ex){}
+                                  }
+                            })
+                        }else{
+                            setBHP("--");setengine("--");setmileage("--")
+                        }
+                    })
+                }
             }
         }
         catch(e){}
@@ -124,8 +138,19 @@ function ModelVariantView(props) {
         },1000)
     },[])
 
+    const capitalize = (s) => {
+        console.log(s)
+        if (typeof s !== 'string') return ''
+        return s.charAt(0).toUpperCase() + s.slice(1)
+      }
+
     return (
         <div style={{ display: "flex", flexDirection: "column", margin: "20px", width: "100%" }}>
+            <Container>
+            <FormGroup style={{marginBottom:"10px"}}>
+                <FormControlLabel label="&nbsp;&nbsp;&nbsp;Back" control={<ArrowBack onClick={() => history.goBack()} />}></FormControlLabel>
+            </FormGroup>
+            </Container>
             <Paper className="keyfeatures">
                 <Container>
                     <Card className="card-root">
@@ -141,11 +166,11 @@ function ModelVariantView(props) {
                         <CardContent>
                             <div style={{ display: "flex", flexDirection: "column", marginLeft: "40px" }}>
                                 <Typography style={{ fontWeight: "bold" }} variant="h5">
-                                    {model.make
-                                     + " "+model.name + " "+ variantName+" " +fuelType}
+                                    {capitalize(model.make)
+                                     + " "+capitalize(model.name) + " "+ capitalize(variant.variantName)+" " +variant.fuelType}
                                </Typography>
-                                <Autocomplete onChange={(e, value) => console.log("uiiu")} size="small"
-                                    style={{ marginRight: "30px", width: 300, marginTop: "30px" }} id="combo-box-de9mo" options={["variantt1", "variant2"]}
+                                <Autocomplete onChange={(e, value) => handleChangeVariant(e,value)} size="small"
+                                    style={{ marginRight: "30px", width: 300, marginTop: "30px" }} id="combo-box-de9mo" options={variantList}
                                     getOptionLabel={option => { return option; }}
                                     disableClearable={true}
                                     renderOption={(option) => (
@@ -172,19 +197,19 @@ function ModelVariantView(props) {
                                     <div style={{ position: "relative", display: "flex", flexDirection: "column" }}>
                                         <Typography variant="body2" color="textSecondary" component="p"> <FlashOnOutlined style={{ fontSize: "40px" }} /></Typography>
                                         <Typography variant="body2" color="textSecondary" component="p" className="spec-icon">BHP</Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p" className="spec-icon-view"></Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p" className="spec-icon-view">{bhp}</Typography>
                                     </div>
 
                                     <div style={{ position: "relative", display: "flex", flexDirection: "column", marginLeft: "60px" }}>
                                         <Typography variant="body2" color="textSecondary" component="p"> <DirectionsCar style={{ fontSize: "40px" }} /></Typography>
                                         <Typography variant="body2" color="textSecondary" component="p" className="spec-icon">Engine(cc)</Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p" className="spec-icon-view">--</Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p" className="spec-icon-view">{engine}</Typography>
                                     </div>
 
                                     <div style={{ position: "relative", display: "flex", flexDirection: "column", marginLeft: "60px" }}>
                                         <Typography variant="body2" color="textSecondary" component="p"> <SpeedOutlined style={{ fontSize: "40px" }} /></Typography>
                                         <Typography variant="body2" color="textSecondary" component="p" className="spec-icon">Mileage</Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p" className="spec-icon-view">--</Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p" className="spec-icon-view">{mileage}</Typography>
                                     </div>
 
                                     <div style={{ position: "relative", display: "flex", flexDirection: "column", marginLeft: "60px" }}>
@@ -203,13 +228,14 @@ function ModelVariantView(props) {
             <div className="variants">
                 <Container>
                     <Typography style={{ fontWeight: "bold", marginBottom: "20px" }} variant="h6">
-                        {model.make + " "+model.name + " "+ variantName+" " } Specifications
+                    {capitalize(model.make)
+                                     + " "+capitalize(model.name) + " "+ capitalize(variant.variantName)+" " +variant.fuelType} Specifications
                    </Typography>
 
               {
                   variant.specifications === undefined ? "":
                   variant.specifications.map((item, index) => {
-                    return <Accordion square  expanded={expanded===item.name } onChange={handleChange(item.name)}>
+                    return <Accordion square  expanded={expanded===item.name} onChange={handleChange(item.name)}>
                      <AccordionSummary expandIcon={<ExpandMore />} aria-controls="panel1d-content" id="panel1d-header">
                      <Typography variant="h6">{item.name}</Typography>
                      </AccordionSummary>
